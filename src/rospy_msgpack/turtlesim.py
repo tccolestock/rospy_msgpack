845df418a78a08c1b7305995d837a58f336915ae
Thomas Colestock <tccolestock@gmail.com>
2016-07-15 23:35:32 -0400

    Added Pose

--- src/rospy_msgpack/turtlesim.py
+++ src/rospy_msgpack/turtlesim.py
@@ -13,6 +13,15 @@ class Encode():
         msg['b'] = data.b
         return(msg)
 
+    def pose(self, data):
+        msg = {}
+        msg['x'] = data.x
+        msg['y'] = data.y
+        msg['theta'] = data.theta
+        msg['lvelo'] = data.linear_velocity
+        msg['avelo'] = data.angular_velocity
+        return(msg)
+
 
 
 class Decode():
@@ -24,4 +33,12 @@ class Decode():
         obj.r = msg['r']
         obj.g = msg['g']
         obj.b = msg['b']
+        return(obj)
+
+    def pose(self, msg, obj):
+        obj.x = msg['x']
+        obj.y = msg['y']
+        obj.theta = msg['theta']
+        obj.linear_velocity = msg['lvelo']
+        obj.angular_velocity = msg['avelo']
         return(obj)
