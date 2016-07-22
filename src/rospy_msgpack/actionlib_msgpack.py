fbcfcc449ee2ac74b16701e1acccc32af49c4489
tccolestock <tccolestock@gmail.com>
2016-07-22 01:58:40 -0400

    Fixed Array handling

--- src/rospy_msgpack/actionlib_msgpack.py
+++ src/rospy_msgpack/actionlib_msgpack.py
@@ -1,5 +1,6 @@
 
 from rospy_msgpack import interpret
+from actionlib_msgs.msg import GoalID, GoalStatus
 
 encode = interpret.Encode()
 decode = interpret.Decode()
@@ -15,16 +16,26 @@ class Encode():
         msg['id'] = data.id
         return(msg)
 
-    # todo: figure out GoalStatus message
+    def goal_status(cls, obj):
+        msg {}
+        t = encode.time_stamp(obj.goal_id.stamp, "")
+        msg['id'] = obj.goal_id.id
+        msg['status'] = obj.status
+        msg['text'] = obj.text
+        msg.update(t)
+        return(msg)
 
     def goal_status_array(cls, obj):
         msg = {}
+        e_time = encode.time_stamp
         h = encode.header(obj.header, "")
-        # msg['seq'] = obj.header.seq
-        # msg['secs'] = obj.header.stamp.secs
-        # msg['nsecs'] = obj.header.stamp.nsecs
-        # msg['frame_id'] = obj.header.frame_id
-        msg['status_list'] = obj.status_list
+        msg['_length'] = len(obj.status_list)
+        for i in range(msg['_length']):
+            t = e_time(obj.status_list[i].goal_id.stamp, i)
+            msg['%s_id' %i] = obj.status_list[i].goal_id.id
+            msg['%s_status' %i] = obj.status_list[i].status
+            msg['%s_text' %i] = obj.status_list[i].text
+            msg.update(t)
         msg.update(h)
         return(msg)
 
@@ -39,13 +50,20 @@ class Decode():
         obj.id = msg['id']
         return(obj)
 
-    # todo: figure out GoalStatus message
+    def goal_status(cls, msg, obj):
+        obj.goal_id.stamp = decode.time_stamp(msg, obj.goal_id.stamp, "")
+        obj.goal_id.id = msg['id']
+        obj.status = msg['status']
+        obj.text = msg['text']
+        return(obj)
 
-    def goal_status_array(self, msg, obj):
+    def goal_status_array(cls, msg, obj):
         obj.header = decode.header(msg, obj.header, "")
-        # obj.header.seq = msg['seq']
-        # obj.header.stamp.secs = msg['secs']
-        # obj.header.stamp.nsecs = msg['nsecs']
-        # obj.header.frame_id = msg['frame_id']
-        obj.status_list = msg['status_list']
+        for i in range(msg['_length']):
+            goal_status = GoalStatus()
+            goal_status.goal_id.stamp = decode.time_stamp(msg, goal_id.stamp, i)
+            goal_status.goal_id.id = msg['%s_id' %i]
+            goal_status.status = msg['%s_status' %i]
+            goal_status.text = msg['%s_text' %i]
+            obj.status_list.append(goal_status)
         return(obj)
